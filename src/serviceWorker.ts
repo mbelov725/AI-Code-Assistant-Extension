f09b24eeff5ec1682526f5353f7055c2589f7329
mbelov725 <mark.belov725@gmail.com>
2026-04-08 23:02:57 -0400

    Restructured files

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,5 +1,3 @@
-const GOOGLE_ORIGIN = "https://www.google.com";
-
 // Allows users to open the side panel by clicking on the action toolbar icon
 chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
