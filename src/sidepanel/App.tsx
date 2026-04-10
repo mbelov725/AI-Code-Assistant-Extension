bd4db39ed5c9a61048f8a20e36120f6b63ee50df
mbelov725 <mark.belov725@gmail.com>
2026-04-10 14:10:36 -0400

    Restructured files

---
+++ src/sidepanel/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <button className="bg-blue-500 text-white p-3 font-bold rounded hover:bg-blue-400">
+        Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
